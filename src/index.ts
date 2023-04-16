f05946302c052705c65d7ecad76178799e2e17d4
D Trombett <maxtromb.dt@gmail.com>
2023-04-16 22:21:11 +0200

    chore: get ready for merge

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { config } from "dotenv";
-import { Client } from ".";
+// import { config } from "dotenv";
+// import { Client } from ".";
 
 export * from "./Client";
 export * from "./api";
@@ -7,14 +7,14 @@ export * from "./structures";
 export * from "./types";
 export * from "./util";
 
-const now = performance.now();
-config();
-const client = new Client({
-	debug: true,
-});
+// const now = performance.now();
+// config();
+// const client = new Client({
+//   debug: true,
+// });
 
-await client.login();
-console.log(await client.getDettagliProfilo(), performance.now() - now);
-// TODO: Add rest with client option
-
+// await client.login();
+// console.log(await client.getDettagliProfilo(), performance.now() - now);
+// TODO: Translate everything
+// TODO: Create utility to generate a code link
 // TODO: Add option to not save local data
