db0d266f796401922ad7af82074676c9088e488c
DTrombett <maxtromb.dt@gmail.com>
2023-05-09 16:09:53 +0200

    deps: move dev deps to deps

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // import { config } from "dotenv";
 // import { Client } from ".";
-//
+
 // config();
 // const now = performance.now();
 // const client = new Client({
 // 	debug: true,
 // });
-//
+
 // client
 // 	.login()
 // 	.then(async () => {
