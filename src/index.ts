e0fc1db778f425c8dee832e75e19bf79090f0143
DTrombett <maxtromb.dt@gmail.com>
2023-05-09 16:34:57 +0200

    chore: remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { config } from "dotenv";
-import { Client } from ".";
+// import { config } from "dotenv";
+// import { Client } from ".";
 
-config();
-const client = new Client({
-	debug: true,
-});
+// config();
+// const client = new Client({
+// 	debug: true,
+// });
 
-client
-	.login()
-	.then(async (newClient) => {
-		console.log(newClient);
-	})
-	.catch(console.error);
+// client
+// 	.login()
+// 	.then(async (newClient) => {
+// 		console.log(newClient);
+// 	})
+// 	.catch(console.error);
 
 export * from "./Client";
 export * from "./api";
