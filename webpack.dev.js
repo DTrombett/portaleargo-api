028ba00530ca2ce82337c021a1fb3eda8a27109d
DTrombett <maxtromb.dt@gmail.com>
2023-11-21 19:17:42 +0100

    fix: skip postinstall for now

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const { builtinModules } = require("node:module");
 
 /**
  * @type {import("webpack").Configuration}
